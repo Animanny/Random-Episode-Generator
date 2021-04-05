dd9479e0ae75f39c9bc58b2c73d4951811270b6f
Ani Srikanth <ani.srikanth23@gmail.com>
2021-04-05 04:43:07 -0400

    Fixed Season Select

--- app.js
+++ app.js
@@ -1,64 +1,124 @@
-const express = require('express');
-const pug = require('pug');
-const request = require('request');
-const util = require('util');
+const express = require("express");
+const pug = require("pug");
+const request = require("request");
+const util = require("util");
 
 const movieDatabase = util.promisify(request);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
-app.use("/static", express.static('./static/'));
+app.use("/static", express.static("./static/"));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
-
-app.post('/getepisode',(req,res)=>{
-  var episode,season,episode_title,episode_description,poster;
-  var show_id; //DEFINING THE ID
-  movieDatabase('https://api.themoviedb.org/3/search/tv?api_key=34bbe0517e6d8f59540de923df8952bc&query='+req.body.show+'&page=1', { json: false }, (err, response, body) => {
-      if (err) { return console.log(err); }
-      try{
-      results = JSON.parse(body);
-      show_id = results.results[0].id;
-      console.log();
-      } catch(error){}
-    movieDatabase('https://api.themoviedb.org/3/tv/'+show_id+'?api_key=34bbe0517e6d8f59540de923df8952bc&language=en-US', { json: false }, (err, response, body) => {
-      if (err) { return console.log(err); }
-      try{
-        results = JSON.parse(body);
-        console.log(results.name);
-        season = Math.floor(Math.random() * results.number_of_seasons)+1;
-        episode = Math.floor(Math.random() * results.seasons[season-1].episode_count)+1;
-      } catch(error){};
-      movieDatabase('https://api.themoviedb.org/3/tv/'+show_id+'/season/'+season+'/episode/'+episode+'?api_key=34bbe0517e6d8f59540de923df8952bc&language=en-US', { json: false }, (err, response, body) => {
-        if (err) { return console.log(err); }
-        try{
-        results = JSON.parse(body);
-        episode_title = results.name;
-        console.log("Title: "+episode_title)
-        episode_description = results.overview;
-        poster = results.still_path;
-        } catch(error){}
-        ep_data = {
-          "season":season,
-          "episode":episode,
-          "desc":episode_description,
-          "poster":poster,
-          "title":episode_title
-        }
-        res.send(ep_data);
-
-        });
-    });
-  });
-})
-
-app.listen(port, () => {
-  console.log(`listening on port ${ port }`);
+app.get("/", (req, res) => {
+	res.render("index");
 });
 
+function getSeason(seasons, season) {
+	for (let i = 0; i < seasons.length; i++) {
+		if (seasons[i].season_number == season) {
+			return i;
+		}
+	}
+}
+
+app.post("/getepisode", (req, res) => {
+	var episode,
+		season,
+		episode_title,
+		episode_description,
+		show_poster,
+		episode_poster;
+	var show_id; //DEFINING THE ID
+	movieDatabase(
+		"https://api.themoviedb.org/3/search/tv?api_key=34bbe0517e6d8f59540de923df8952bc&query=" +
+			req.body.show +
+			"&page=1",
+		{ json: false },
+		(err, response, body) => {
+			if (err) {
+				return console.log(err);
+			}
+			try {
+				results = JSON.parse(body);
+				show_id = results.results[0].id;
+			} catch (error) {}
+			movieDatabase(
+				"https://api.themoviedb.org/3/tv/" +
+					show_id +
+					"?api_key=34bbe0517e6d8f59540de923df8952bc&language=en-US",
+				{ json: false },
+				(err, response, body) => {
+					if (err) {
+						return console.log(err);
+					}
+					try {
+						results = JSON.parse(body);
+
+						show_poster = results["backdrop_path"];
+						season =
+							Math.floor(
+								Math.random() *
+									(results.number_of_seasons - 1 + 1)
+							) + 1;
+
+						episode =
+							Math.floor(
+								Math.random() *
+									(results.seasons[
+										getSeason(results.seasons, season)
+									].episode_count -
+										1 +
+										1)
+							) + 1;
+					} catch (error) {}
+					movieDatabase(
+						"https://api.themoviedb.org/3/tv/" +
+							show_id +
+							"/season/" +
+							season +
+							"/episode/" +
+							episode +
+							"?api_key=34bbe0517e6d8f59540de923df8952bc&language=en-US",
+						{ json: false },
+						(err, response, body) => {
+							if (err) {
+								return console.log(err);
+							}
+							try {
+								results = JSON.parse(body);
+								episode_title = results.name;
+								console.log(
+									"S" +
+										season +
+										"E" +
+										episode +
+										" - Title: " +
+										episode_title
+								);
+								episode_description = results.overview;
+								episode_poster = results.still_path;
+							} catch (error) {}
+							ep_data = {
+								season: season,
+								episode: episode,
+								desc: episode_description,
+								episode_poster: episode_poster,
+								show_poster: show_poster,
+								title: episode_title,
+							};
+							res.send(ep_data);
+						}
+					);
+				}
+			);
+		}
+	);
+});
+
+app.listen(port, () => {
+	console.log(`listening on port ${port}`);
+});
--- static/script.js
+++ static/script.js
@@ -1,51 +1,66 @@
-function getEpisode(){
-    let showname = document.getElementById('showNameInput').value;
+function getEpisode() {
+	let showinfo;
+	let showname = document.getElementById("showNameInput").value;
 
-    episode = {
-        "show": document.getElementById('showNameInput').value
-    }
+	episode = {
+		show: document.getElementById("showNameInput").value,
+	};
 
-    console.log(episode);
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			showinfo = JSON.parse(this.responseText);
+			if (showinfo.season != undefined && showinfo.episode != undefined) {
+				document.getElementById("episodenumber").innerHTML =
+					"Season: " +
+					showinfo.season +
+					" Episode: " +
+					showinfo.episode;
 
+				if (showinfo.desc) {
+					document.getElementById("episodedesc").innerHTML =
+						showinfo.desc;
+				} else {
+					document.getElementById("episodedesc").innerHTML =
+						"Decription not available.";
+				}
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-         if (this.readyState == 4 && this.status == 200) {
-             //alert(this.responseText);
-             showinfo = JSON.parse(this.responseText);
-             if(showinfo.season != undefined && showinfo.episode != undefined){
-                document.getElementById('episodenumber').innerHTML = "Season: "+showinfo.season+" Episode: "+showinfo.episode;
-                if(showinfo.desc != undefined){
-                    document.getElementById('episodedesc').innerHTML = showinfo.desc;
-                    if(showinfo.title != undefined){
-                        document.getElementById('episodetitle').innerHTML = showinfo.title;
-                        if(showinfo.poster != undefined){
-                            document.getElementById('epImage').src = "https://image.tmdb.org/t/p/original"+showinfo.poster;
-                        } else {
-                            document.getElementById('epImage').src ="";
-                        }
-                    } else {
-                        document.getElementById('episodetitle').innerHTML = ""
-                    }
-                }else{
-                    document.getElementById('episodedesc').innerHTML = "Decription not available."
-                }
-             } else {
-                document.getElementById('episodenumber').innerHTML = "Sorry, we weren't able to find that show";
-                document.getElementById('episodetitle').innerHTML = "";
-                document.getElementById('episodedesc').innerHTML = "";
-                document.getElementById('epImage').src ="";
+				if (showinfo.title != undefined) {
+					document.getElementById("episodetitle").innerHTML =
+						showinfo.title;
+				} else {
+					document.getElementById("episodetitle").innerHTML = "";
+				}
 
-            }
-         }
-    };
-    xhttp.open("POST", "/getepisode", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(JSON.stringify(episode));
+				if (showinfo.episode_poster != undefined) {
+					document.getElementById("epImage").src =
+						"https://image.tmdb.org/t/p/original" +
+						showinfo.episode_poster;
+				} else {
+					if (showinfo.show_poster != undefined) {
+						document.getElementById("epImage").src =
+							"https://image.tmdb.org/t/p/original" +
+							showinfo.show_poster;
+					} else {
+						document.getElementById("epImage").src = null;
+					}
+				}
+			} else {
+				document.getElementById("episodenumber").innerHTML =
+					"Sorry, we weren't able to find that show";
+				document.getElementById("episodetitle").innerHTML = "";
+				document.getElementById("episodedesc").innerHTML = "";
+				document.getElementById("epImage").src = "";
+			}
+		}
+	};
+	xhttp.open("POST", "/getepisode", true);
+	xhttp.setRequestHeader("Content-type", "application/json");
+	xhttp.send(JSON.stringify(episode));
 }
 
 function search(ele) {
-    if(event.key === 'Enter') {
-        getEpisode();        
-    }
+	if (event.key === "Enter") {
+		getEpisode();
+	}
 }
